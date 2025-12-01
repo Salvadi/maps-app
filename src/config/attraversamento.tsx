5881b0115b0ea3e3f7db263a33aedc449968b026
Salvadi <107889911+Salvadi@users.noreply.github.com>
2025-12-01 16:24:17 +0100

    Update ATTRAVERSAMENTO_OPTIONS with new values

--- src/config/attraversamento.tsx
+++ src/config/attraversamento.tsx
@@ -5,16 +5,27 @@ export interface MenuOption {
 
 export const ATTRAVERSAMENTO_OPTIONS: MenuOption[] = [
   { value: '', label: '' },
-  { value: 'cavi_corrugati', label: 'Cavi/Corrugati' },
-  { value: 'fascio_cavi', label: 'Fascio di cavi' },
-  { value: 'canalina_passacavi', label: 'Canalina passacavi' },
-  { value: 'tubo_combustibile', label: 'Tubo combustibile' },
-  { value: 'tubo_multistrato', label: 'Tubo multistrato' },
-  { value: 'tubo_incombustibile_nudo', label: 'Tubo incombustibile NUDO' },
-  { value: 'tubo_incombustibile_isolato', label: 'Tubo incombustibile ISOLATO combustibile' },
-  { value: 'tubo_rame_isolato', label: 'Tubo RAME isolato' },
-  { value: 'tubo_areazione_lamiera', label: 'Tubo areazione in lamiera' },
-  { value: 'tubo_areazione_spiralato', label: 'Tubo areazione spiralato' },
-  { value: 'serranda', label: 'Serranda' },
-  { value: 'asola', label: 'Asola' },
+  { value: 'Cavi/Corrugati', label: 'Cavi/Corrugati' },
+  { value: 'Fascio di cavi', label: 'Fascio di cavi' },
+  { value: 'Canalina passacavi', label: 'Canalina passacavi' },
+  
+  { value: 'Tubo combustibile', label: 'Tubo combustibile' },
+  { value: 'Tubo multistrato', label: 'Tubo multistrato' },
+  
+  { value: 'Tubo metallico NUDO', label: 'Tubo metallico NUDO' },
+  { value: 'Tubo metallico ISOLATO Armaflex', label: 'Tubo metallico ISOLATO Armaflex' },
+  { value: 'Tubo metallico ISOLATO lana', label: 'Tubo metallico ISOLATO lana' },
+
+  { value: 'Tubo RAME isolato Armaflex', label: 'Tubo RAME isolato Armaflex' },
+  { value: 'Tubo RAME nudo', label: 'Tubo RAME nudo' },
+
+  { value: 'Tubo areazione in lamiera', label: 'Tubo areazione in lamiera' },
+  { value: 'Tubo areazione spiralato', label: 'Tubo areazione spiralato' },
+
+  { value: 'Serranda', label: 'Serranda' },
+  { value: 'Canala areazione', label: 'Canala areazione' },
+
+  { value: 'Asola', label: 'Asola' },
+
+  { value: 'Altro', label: 'Altro' },
 ];
