ef6d3eac87ac6eb05f452780ab4c2382ac3176bc
Salvadi <107889911+Salvadi@users.noreply.github.com>
2025-12-01 16:05:44 +0100

    Update TIPO_SUPPORTO_OPTIONS with new values

--- src/config/tipoSupporto.tsx
+++ src/config/tipoSupporto.tsx
@@ -5,9 +5,9 @@ export interface MenuOption {
 
 export const TIPO_SUPPORTO_OPTIONS: MenuOption[] = [
   { value: '', label: '' },
-  { value: 'brick', label: 'Mattoni' },
-  { value: 'concrete', label: 'Cemento' },
-  { value: 'wood', label: 'Legno' },
-  { value: 'steel', label: 'Acciaio' },
-  { value: 'plasterboard', label: 'Cartongesso' },
+  { value: 'Cartongesso', label: 'Cartongesso' },
+  { value: 'Cemento', label: 'Cemento' },
+  { value: 'Laterizio intonacato', label: 'Laterizio intonacato' },
+  { value: 'Laterizio NON intonacato', label: 'Laterizio NON intonacato' },
+  { value: 'Legno', label: 'Legno' },
 ];
